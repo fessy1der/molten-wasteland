8ab44dd0c64fda4a9547afde365f2a8cd39710cb
Festus Obi <obi@Obis-MacBook-Pro-2.local>
2024-08-23 21:52:05 +0200

    fixed issue with grid

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -10,7 +10,8 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
+        // "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
+        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
         className
       )}
     >
@@ -41,9 +42,13 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 border-orange-600",
+        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-orange-600/[0.5] bg-white border border-transparent justify-between flex flex-col space-y-4 border-orange-600",
         className
       )}
+      style={{
+        background: 'rgb(0,0,0)',
+        backgroundColor: 'gradient...'
+      }}
     >
       <div className="group-hover/bento:translate-x-2 transition duration-200">
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
