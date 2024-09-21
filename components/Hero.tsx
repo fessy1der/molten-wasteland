90f913d9ad3e59b54ebdce5bdff46879e4d0714e
Festus obi <fessy_wonder@yahoo.co.uk>
2024-09-21 11:13:16 +0200

    added hero image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,7 @@ import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
 import MagicButton from './ui/MagicButton'
 import { FaLink, FaLocationArrow } from 'react-icons/fa6'
+import backgroundImage from '../public/empty_background.png';
 
 const Hero = () => {
   return (
@@ -12,7 +13,7 @@ const Hero = () => {
         <Spotlight className='top-10 left-full h-[60vh] w-[50vw]' fill='orange'/>
         <Spotlight className='top-28 left-80 h-[60vh] w-[50vw]' fill='orange'/>
       </div>
-      <div className="h-screen w-full dark:bg-black bg-white  dark:bg-grid-white/[0.02] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+      <div id='hero-section' className="h-screen w-full dark:bg-black bg-white dark:bg-grid-white/[0.02] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
     </div>
     <div className='flex justify-center relative my-20 z-10'>
